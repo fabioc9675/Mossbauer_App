2c8c0884a22f131790268758ad18ca1a3fa0d947
fabioc9675 <fabian.castano@udea.edu.co>
2023-08-01 00:09:37 -0500

    Implementation of tensorflow prediction for Mossbauer

---
+++ Mosbauer_App/Mossbauer_App/views.py
@@ -0,0 +1,61 @@
+from typing import Optional
+from django.conf import settings
+from django.template.response import TemplateResponse
+from django.utils.datastructures import MultiValueDictKeyError
+
+from django.core.files.storage import FileSystemStorage
+
+import tensorflow as tf
+import numpy as np
+import os
+
+
+class CustomFileSystemStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        self.delete(name)
+        return name
+
+
+def index(request):
+    message = ""
+    prediction = ""
+    fss = CustomFileSystemStorage()
+    try:
+
+        # load model
+        model = tf.keras.models.load_model(
+            os.getcwd() + os.path.join(os.sep, "model", "Mossbauer_model.h5"))
+
+        spectrum = request.FILES["file"]
+        print("Name", spectrum.file)
+        _spectrum = fss.save(spectrum.name, spectrum)
+        path = str(settings.MEDIA_ROOT) + "/" + spectrum.name
+        # read the spectrum
+        spec_ = -np.loadtxt(path, dtype=float)[:, 1] / 10
+        spec_ = spec_.tolist()
+
+        spectrum_pred = [spec_]
+        categ = ["Otro", "Hematita", "Magnetita"]
+        result = np.argmax(model.predict(spectrum_pred), axis=-1)
+
+        print("Prediction: " + str(np.argmax(result)))
+
+        prediction = categ[result[0]]
+
+        return TemplateResponse(
+            request,
+            "index.html",
+            {
+                "message": message,
+                "spectrum": spectrum_pred,
+                "prediction": prediction
+            }
+        )
+    except MultiValueDictKeyError:
+        return TemplateResponse(
+            request,
+            "index.html",
+            {
+                "message": "No File Selected"
+            },
+        )
